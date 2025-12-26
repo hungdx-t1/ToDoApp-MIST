9202b425ceeb8b77def3333018e4b56e77df419e
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2025-12-26 13:11:12 +0700

    moved TaskSearchDelegate to util package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
-import 'package:todoapp_mist/services/notification_service.dart';
+import 'services/notification_service.dart';
+import 'utils/task_search_delegate.dart';
 import 'view_models/task_view_model.dart';
 import 'views/add_task_screen.dart';
 import 'views/task_detail_screen.dart';
@@ -281,46 +282,5 @@ class _HomeScreenState extends State<HomeScreen> {
         );
       },
     );
-  }
-}
-
-// Lớp xử lý Tìm kiếm (Delegate)
-class TaskSearchDelegate extends SearchDelegate {
-  @override
-  String? get searchFieldLabel => 'Tìm kiếm công việc...';
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = ''; // Xóa text tìm kiếm
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null); // Đóng màn hình tìm kiếm
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    // Logic hiển thị kết quả khi nhấn Enter
-    return Center(child: Text("Kết quả cho: $query"));
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    // Logic hiển thị gợi ý khi đang gõ
-    // Bạn có thể kết nối với ViewModel để filter list task tại đây
-    return Container();
   }
 }
---
+++ lib/utils/task_search_delegate.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+// lớp xử lý tìm kiếm
+class TaskSearchDelegate extends SearchDelegate {
+  @override
+  String? get searchFieldLabel => 'Tìm kiếm công việc...';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = ''; // Xóa text tìm kiếm
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null); // Đóng màn hình tìm kiếm
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Logic hiển thị kết quả khi nhấn Enter
+    return Center(child: Text("Kết quả cho: $query"));
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Logic hiển thị gợi ý khi đang gõ
+    // Bạn có thể kết nối với ViewModel để filter list task tại đây
+    return Container();
+  }
+}
